39476dff1cc0deb04cc02c2118a8f15c6ff43335
Vaddiraju Surya Teja <112264612+suryavaddiraju@users.noreply.github.com>
2023-12-15 07:07:07 +0530

    Update __version__.py

--- src/sesparser/__version__.py
+++ src/sesparser/__version__.py
@@ -5,7 +5,7 @@
 __title__ = "sesparser_suryavaddiraju"
 __description__ = "SES Incoming email Parser"
 __url__ = "https://requests.readthedocs.io"
-__version__ = "1.9.2"
+__version__ = "1.9.3"
 __author__ = "Vaddiraju Surya Teja"
 __author_email__ = "suryavaddiraju@proton.me"
 __license__ = "Apache-2.0"
