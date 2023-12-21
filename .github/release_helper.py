2eab033486915f0fe41481d2389a1a7c00cea44c
Vaddiraju Surya Teja <suryavaddiraju@proton.me>
2023-12-22 04:53:45 +0530

    imao

--- .github/release_helper.py
+++ .github/release_helper.py
@@ -3,7 +3,7 @@ import sys
 current_dir = os.getcwd()
 
 # Construct the relative path
-relative_path = os.path.join(current_dir, './src/')
+relative_path = os.path.join(current_dir, './src/sesparser/')
 
 # Get the absolute path
 absolute_path = os.path.abspath(relative_path)
@@ -11,7 +11,7 @@ sys.path.insert(0, absolute_path)
 print("Current directory:", current_dir)
 print("Absolute path to 'src':", absolute_path)
 print("sys.path:", sys.path)
-from sesparser.__version__ import(
+from __version__ import(
     __version__
 )
 release_notes = f"A stable public version {__version__} has been released for the python package **sesparser** which is available online at python packages [pypi library](https://pypi.org/project/sesparser/)\nThe latest version of the module can be installed through this below pip command:\n```pip install sesparser```\nIf you need to install it offline, then first download the [sesparser-{__version__}-py3-none-any.whl](https://github.com/suryavaddiraju/sesparser/releases/download/v{__version__}/sesparser-{__version__}-py3-none-any.whl) file attached in the assets part of this release note. then use this below pip command\n```pip install path/to/the/downloaded/whl/file```\nAdditional release note for this version is made available at the [HISTORY.MD](/HISTORY.MD) file located in this [github repository](https://github.com/suryavaddiraju/sesparser) root folder."
