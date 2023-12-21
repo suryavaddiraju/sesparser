ce0921f5ea9226084743601cf9cacbe009fc1214
Vaddiraju Surya Teja <suryavaddiraju@proton.me>
2023-12-22 05:17:52 +0530

    newww

--- .github/release_helper.py
+++ .github/release_helper.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import uuid
 current_dir = os.getcwd()
 
 # Construct the relative path
@@ -14,6 +15,25 @@ print("sys.path:", sys.path)
 from __version__ import(
     __version__
 )
-release_notes = f"A stable public version {__version__} has been released for the python package **sesparser** which is available online at python packages [pypi library](https://pypi.org/project/sesparser/)\nThe latest version of the module can be installed through this below pip command:\n```pip install sesparser```\nIf you need to install it offline, then first download the [sesparser-{__version__}-py3-none-any.whl](https://github.com/suryavaddiraju/sesparser/releases/download/v{__version__}/sesparser-{__version__}-py3-none-any.whl) file attached in the assets part of this release note. then use this below pip command\n```pip install path/to/the/downloaded/whl/file```\nAdditional release note for this version is made available at the [HISTORY.MD](/HISTORY.MD) file located in this [github repository](https://github.com/suryavaddiraju/sesparser) root folder."
-print(f'"version={__version__}" >> $GITHUB_OUTPUT')
-print(f'"release_notes={release_notes}" >> $GITHUB_OUTPUT')
+release_notes = f"""A stable public version {__version__} has been released for the python package **sesparser** which is available online at python packages [pypi library](https://pypi.org/project/sesparser/)
+The latest version of the module can be installed through this below pip command:
+```pip install sesparser```
+If you need to install it offline, then first download the [sesparser-{__version__}-py3-none-any.whl](https://github.com/suryavaddiraju/sesparser/releases/download/v{__version__}/sesparser-{__version__}-py3-none-any.whl) file attached in the assets part of this release note. then use this below pip command
+```pip install path/to/the/downloaded/whl/file```
+Additional release note for this version is made available at the [HISTORY.MD](/HISTORY.MD) file located in this [github repository](https://github.com/suryavaddiraju/sesparser) root folder."""
+
+
+def set_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'{name}={value}', file=fh)
+
+
+def set_multiline_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        delimiter = uuid.uuid1()
+        print(f'{name}<<{delimiter}', file=fh)
+        print(value, file=fh)
+        print(delimiter, file=fh)
+
+set_output("version",__version__)
+set_multiline_output("release_notes",release_notes)
